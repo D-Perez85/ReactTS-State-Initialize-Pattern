b1dc05d913c523c2a717730f667ca44e82e64a95
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-11 17:38:08 -0300

    ProductTitle component

--- src/state-initializer-pattern/pages/ShoppingPage.tsx
+++ src/state-initializer-pattern/pages/ShoppingPage.tsx
@@ -1,11 +1,12 @@
 import { ProductCard } from "../components/ProductCard";
 import { ProductImg } from "../components/ProductImage";
+import { ProductTitle } from "../components/ProductTitle";
 import { ProductButtons } from '../components/ProductButtons';
 import "../styles/custom-styles.css";
 
 const product = {
   id: "1",
-  title: "Delicious Cafe",
+  title: "Delicious Coffe",
   img: "./coffee-mug.png",
 };
 
@@ -17,6 +18,7 @@ const ShoppingPage = () => {
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
       <ProductCard product={product} className="bg-dark text-white">
           <ProductImg className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
+          <ProductTitle title={product.title} className="text-white text-bold"/>
           <ProductButtons className="custom-buttons"/>
       </ProductCard>
       </div>
