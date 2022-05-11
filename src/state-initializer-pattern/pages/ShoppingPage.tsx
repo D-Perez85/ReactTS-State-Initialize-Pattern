9df39b5add349814c4989c24a6e680cdf3ae9a7c
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-11 17:47:52 -0300

    Dynamic ProductCard

--- src/state-initializer-pattern/pages/ShoppingPage.tsx
+++ src/state-initializer-pattern/pages/ShoppingPage.tsx
@@ -3,12 +3,7 @@ import { ProductImg } from "../components/ProductImage";
 import { ProductTitle } from "../components/ProductTitle";
 import { ProductButtons } from '../components/ProductButtons';
 import "../styles/custom-styles.css";
-
-const product = {
-  id: "1",
-  title: "Delicious Coffe",
-  img: "./coffee-mug.png",
-};
+import { products } from "../data/products";
 
 const ShoppingPage = () => {
   return (
@@ -16,11 +11,13 @@ const ShoppingPage = () => {
       <h1> Shopping Store </h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-      <ProductCard product={product} className="bg-dark text-white">
-          <ProductImg className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
-          <ProductTitle title={product.title} className="text-white text-bold"/>
-          <ProductButtons className="custom-buttons"/>
-      </ProductCard>
+       {products.map((product)=>(
+          <ProductCard product={product} className="bg-dark text-white">
+              <ProductImg className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
+              <ProductTitle title={product.title} className="text-white text-bold"/>
+              <ProductButtons className="custom-buttons"/>
+          </ProductCard>
+          ))}
       </div>
     </div>
   );
